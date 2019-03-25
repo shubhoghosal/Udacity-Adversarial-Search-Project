e38ab57f139ff329f7f10f1244e3e77682d41c79
SG <shubhoghosal@users.noreply.github.com>
2019-03-25 18:31:13 -0400

    Add files via upload

---
+++ alpha_beta_search.py
@@ -0,0 +1,58 @@
+def alpha_beta_search(gameState):
+    """ Return the move along a branch of the game tree that
+    has the best possible value.  A move is a pair of coordinates
+    in (column, row) order corresponding to a legal move for
+    the searching player.
+    
+    You can ignore the special case of calling this function
+    from a terminal state.
+    """
+    alpha = float("-inf")
+    beta = float("inf")
+    best_score = float("-inf")
+    best_move = None
+    for a in gameState.actions():
+        v = min_value(gameState.result(a), alpha, beta)
+        alpha = max(alpha, v)
+        if v > best_score:
+            best_score = v
+            best_move = a
+    return best_move
+
+# TODO: modify the function signature to accept an alpha and beta parameter
+def min_value(gameState, alpha, beta):
+    """ Return the value for a win (+1) if the game is over,
+    otherwise return the minimum value over all legal child
+    nodes.
+    """
+    if gameState.terminal_test():
+        return gameState.utility(0)
+    
+    v = float("inf")
+    for a in gameState.actions():
+        # TODO: modify the call to max_value()
+        v = min(v, max_value(gameState.result(a), alpha, beta))
+        # TODO: update the value bound
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
+    return v
+
+# TODO: modify the function signature to accept an alpha and beta parameter
+def max_value(gameState, alpha, beta):
+    """ Return the value for a loss (-1) if the game is over,
+    otherwise return the maximum value over all legal child
+    nodes.
+    """
+    if gameState.terminal_test():
+        return gameState.utility(0)
+    
+    v = float("-inf")
+    for a in gameState.actions():
+        # TODO: modify the call to min_value()
+        v = max(v, min_value(gameState.result(a), alpha, beta))
+        # TODO: update the value bound
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+    return v
